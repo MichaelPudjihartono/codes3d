1ddb8af683de4a159ce712eabefba5b6ce635475
MichaelPudjihartono <michaelpudj@gmail.com>
2021-12-15 11:57:40 +1300

    Can now accept multiple reference file for each feature and process them automatically (as long as the reference file has the word H3K27ac, H3K4me1, or DNase as appropriate)

--- codes3d/features.py
+++ codes3d/features.py
@@ -21,11 +21,17 @@ def feature_intersect(inputs, output_dir, logger):   #inputs is specified as snp
     #Intersecting with H3K27ac
 	logger.write('---> Looking for intersections with H3K27ac peaks...')
     
-    #1. read the H3K27ac reference file into a BedTool object
-	H3K27ac_bed = pybedtools.BedTool(os.path.join(os.path.dirname(__file__), '../reference_file2/E059-H3K27ac.broadPeak')) #Relative path from SNP_loading.py to the H3K27ac reference file
+    #1. read all of the H3K27ac reference files into BedTool objects stored within dictionary keys
+	dictionary = {}
+	for filename in os.listdir(os.path.join(os.path.dirname(__file__),'../reference_file2/')):
+		if "H3K27ac" in filename:
+			f = os.path.join(os.path.join(os.path.dirname(__file__),'../reference_file2/'), filename)
+			dictionary[filename] = pybedtools.BedTool(f)
+	
+	list = dictionary.values()
     
-    #2. Intersect the 'a' file(snp_bed) with the 'b' file(reference file: H3K27ac bed), and save the output .bed file to the user-specified args.output_dir!
-	snp_bed.intersect(H3K27ac_bed, u=True).saveas(os.path.join(output_dir, 'snps_that_intersects_H3K27ac.bed'))
+    #2. Intersect the 'a' file(snp_bed) with all of the 'b' files(reference file: H3K27ac broadpeak), and save the output .bed file to the user-specified args.output_dir!
+	snp_bed.intersect(list, u = True).saveas(os.path.join(output_dir, 'snps_that_intersects_H3K27ac.bed'))
     
     #3. Next, read back the output file into a Data Frame!
 	snps_that_intersects_H3K27ac = pd.read_csv(os.path.join(output_dir, 'snps_that_intersects_H3K27ac.bed'), sep = '\t', header = None)
@@ -44,8 +50,14 @@ def feature_intersect(inputs, output_dir, logger):   #inputs is specified as snp
     
     #Intersecting with H3K4me1
 	logger.write('---> Looking for intersections with H3K4me1 peaks...')
-	H3K4me1_bed = pybedtools.BedTool(os.path.join(os.path.dirname(__file__), '../reference_file2/GSM958152_UCSF-UBC.Penis_Foreskin_Melanocyte_Primary_Cells.H3K4me1.skin03.bed'))
-	snp_bed.intersect(H3K4me1_bed, u=True).saveas(os.path.join(output_dir, 'snps_that_intersects_H3K4me1.bed'))
+	dictionary = {}
+	for filename in os.listdir(os.path.join(os.path.dirname(__file__),'../reference_file2/')):
+		if "H3K4me1" in filename:
+			f = os.path.join(os.path.join(os.path.dirname(__file__),'../reference_file2/'), filename)
+			dictionary[filename] = pybedtools.BedTool(f)
+	list = dictionary.values()
+
+	snp_bed.intersect(list, u=True).saveas(os.path.join(output_dir, 'snps_that_intersects_H3K4me1.bed'))
 	snps_that_intersects_H3K4me1 = pd.read_csv(os.path.join(output_dir, 'snps_that_intersects_H3K4me1.bed'), sep = '\t', header = None)
 	snps_that_intersects_H3K4me1_list = snps_that_intersects_H3K4me1[2].drop_duplicates().tolist()
 	for index, row in snp_df.iterrows():
@@ -59,8 +71,14 @@ def feature_intersect(inputs, output_dir, logger):   #inputs is specified as snp
 
     #Intersecting with DNase hypersensitivity sites
 	logger.write('---> Looking for intersections with DNase hypersensitive sites...')
-	DNase_bed = pybedtools.BedTool(os.path.join(os.path.dirname(__file__), '../reference_file2/GSM774244_UW.Penis_Foreskin_Melanocyte_Primary_Cells.ChromatinAccessibility.skin01.DS18601.bed'))
-	snp_bed.intersect(DNase_bed, u=True).saveas(os.path.join(output_dir, 'snps_that_intersects_DNase.bed'))
+	dictionary = {}
+	for filename in os.listdir(os.path.join(os.path.dirname(__file__),'../reference_file2/')):
+		if "DNase" in filename:
+			f = os.path.join(os.path.join(os.path.dirname(__file__),'../reference_file2/'), filename)
+			dictionary[filename] = pybedtools.BedTool(f)
+	list = dictionary.values()
+
+	snp_bed.intersect(list, u=True).saveas(os.path.join(output_dir, 'snps_that_intersects_DNase.bed'))
 	snps_that_intersects_DNase = pd.read_csv(os.path.join(output_dir, 'snps_that_intersects_DNase.bed'), sep = '\t', header = None)
 	snps_that_intersects_DNase_list = snps_that_intersects_DNase[2].drop_duplicates().tolist()
 	for index, row in snp_df.iterrows():
